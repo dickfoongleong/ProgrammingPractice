e76ce30298f8fb1ef87a888170dac84a0ce5c1ce
DickFoong Leong <dickfoong0314@gmail.com>
2020-06-25 13:39:02 -0400

    Added UI to the client windows.

--- Java/SocketProgramming/src/Client.java
+++ Java/SocketProgramming/src/Client.java
@@ -1,72 +1,246 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.InetAddress;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
-public class Client {
-	private Socket socket;
-	private PrintWriter output;
-	private BufferedReader input;
+public class Client extends JFrame implements WindowListener {
+  private static final long serialVersionUID = 1L;
+  private static final String SERVER_IP = "10.0.0.18";
+  private static final List<String> ONLINE_USERS = new ArrayList<String>();
 
-	public Client() {
-		try {
-			String ip = "10.0.0.118";
-			socket = new Socket(ip, 9000);
+  private JScrollPane outputPane, userPane;
+  private JTextArea inputTextArea;
 
-			output = new PrintWriter(socket.getOutputStream(), true);
-			input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+  private Socket socket;
+  private PrintWriter output;
+  private BufferedReader input;
 
-			Thread inputReader = new Thread(new Runnable() {
-				public void run() {
-					try {
-						msgFromServer();
-					} catch (Exception e) {
-						System.exit(0);
-					}
-				}
-			});
+  public Client(String username) {
+    addWindowListener(this);
+    setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+    setResizable(false);
+    setTitle(username + " Chat Room");
 
-			Thread outputWriter = new Thread(new Runnable() {
-				public void run() {
-					try {
-						msgToServer();
-					} catch (Exception e) {
-						System.exit(0);
-					}
-				}
-			});
+    JPanel contentPanel = new JPanel();
+    contentPanel.setLayout(new BorderLayout());
+    contentPanel.setBorder(BorderFactory.createEmptyBorder(3, 3, 3, 3));
+    setContentPane(contentPanel);
 
-			inputReader.start();
-			outputWriter.start();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    initComponents();
 
-	private void msgFromServer() throws Exception {
-		String line = "";
-		while ((line = input.readLine()) != null) {
-			System.out.println("Server: " + line);
-		}
-		System.exit(0);
-	}
+    JPanel mainPanel = new JPanel();
+    mainPanel.setLayout(new BorderLayout());
+    mainPanel.add(outputPane, BorderLayout.CENTER);
+    mainPanel.add(userPane, BorderLayout.EAST);
+    contentPanel.add(mainPanel, BorderLayout.CENTER);
 
-	private void msgToServer() throws Exception {
-		String line = "";
-		Scanner scanner = new Scanner(System.in);
-		while (!line.contains("exit")) {
-			line = scanner.nextLine();
-			if (!line.trim().isEmpty()) {
-				output.println(line);
-			}
-		}
-		scanner.close();
-		System.exit(0);
-	}
+    contentPanel.add(msgInputComp(), BorderLayout.SOUTH);
 
-	public static void main(String[] args) {
-		new Client();
-	}
+    Dimension size = new Dimension(900, 500);
+    setPreferredSize(size);
+    setMaximumSize(size);
+    setMinimumSize(size);
+
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    int scMidWidth = screenSize.width / 2;
+    int scMidHeight = screenSize.height / 2;
+    int frameMidWidth = size.width / 2;
+    int frameMidHeight = size.height / 2;
+    setLocation(scMidWidth - frameMidWidth, scMidHeight - frameMidHeight);
+
+    setVisible(true);
+    inputTextArea.requestFocus();
+
+    try {
+      socket = new Socket(SERVER_IP, 9000);
+
+      output = new PrintWriter(socket.getOutputStream(), true);
+      input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      
+      Thread inputReader = new Thread(new Runnable() {
+        public void run() {
+          try {
+            msgFromServer();
+          } catch (Exception e) {
+            e.printStackTrace();
+          }
+        }
+      });
+
+      inputReader.start();
+      msgToServer(username);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void initComponents() {
+    outputPane = new JScrollPane();
+    JTextArea outputArea = new JTextArea();
+    outputArea.setEditable(false);
+
+    outputPane.setViewportView(outputArea);
+
+    userPane = new JScrollPane();
+    userPane.setPreferredSize(new Dimension(200, 0));
+    userPane.setMaximumSize(new Dimension(200, 0));
+    userPane.setMinimumSize(new Dimension(200, 0));
+    updateUserList();
+  }
+
+  private JPanel msgInputComp() {
+    JPanel msgPanel = new JPanel();
+    msgPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+    inputTextArea = new JTextArea();
+    JScrollPane msgPane = new JScrollPane(inputTextArea);
+    msgPane.setPreferredSize(new Dimension(500, 50));
+    msgPanel.add(msgPane);
+
+    JButton enterBtn = new JButton("Send");
+    enterBtn.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent evt) {
+        String msg = inputTextArea.getText();
+        if (!msg.trim().isEmpty()) {
+          try {
+            msgToServer(msg);
+            inputTextArea.setText("");
+          } catch (Exception e) {
+            e.printStackTrace();
+          }
+        }
+        inputTextArea.requestFocus();
+      }
+    });
+    msgPanel.add(enterBtn);
+
+    return msgPanel;
+  }
+
+  private void updateMsg(String msg) {
+    JTextArea textArea = (JTextArea) outputPane.getViewport().getView();
+    String text = textArea.getText();
+    textArea.setText(text + msg + "\n");
+
+    JScrollBar verticalBar = outputPane.getVerticalScrollBar();
+    verticalBar.setValue(verticalBar.getMaximum());
+    revalidate();
+    repaint();
+  }
+
+  private void updateUserList() {
+    JPanel userPanel = new JPanel();
+    userPanel.setOpaque(true);
+    userPanel.setBackground(Color.WHITE);
+    userPanel.setLayout(new BoxLayout(userPanel, BoxLayout.Y_AXIS));
+    for (String user : ONLINE_USERS) {
+      userPanel.add(new JLabel(user));
+    }
+    userPane.setViewportView(userPanel);
+  }
+
+  private void msgFromServer() throws Exception {
+    String line = "";
+    while ((line = input.readLine()) != null) {
+      if (line.startsWith("******") && line.endsWith("has joined ******")) {
+        String newUser = line.substring(7, line.length() - 18);
+        
+        if (!ONLINE_USERS.contains(newUser)) {
+          updateMsg(line);
+          ONLINE_USERS.add(newUser);
+        }
+        updateUserList();
+      } else if (line.startsWith("******") && line.endsWith("has left ******")) {
+        String rmvUser = line.substring(7, line.length() - 16);
+        ONLINE_USERS.remove(rmvUser);
+        updateUserList();
+        
+        updateMsg(line);
+      } else {
+        updateMsg(line); 
+      }
+    }
+  }
+
+  private void msgToServer(String msg) throws Exception {
+    output.println(msg);
+  }
+
+  @Override
+  public void windowOpened(WindowEvent e) {
+  }
+
+  @Override
+  public void windowClosing(WindowEvent e) {
+    System.exit(0);
+  }
+
+  @Override
+  public void windowClosed(WindowEvent e) {
+  }
+
+  @Override
+  public void windowIconified(WindowEvent e) {
+
+  }
+
+  @Override
+  public void windowDeiconified(WindowEvent e) {
+
+  }
+
+  @Override
+  public void windowActivated(WindowEvent e) {
+
+  }
+
+  @Override
+  public void windowDeactivated(WindowEvent e) {
+
+  }
+
+  public static void main(String[] args) {
+    while (true) {
+      final String username = JOptionPane.showInputDialog("Please enter your name:");
+
+      if (username == null) {
+        System.exit(0);
+      } else if (username.trim().isEmpty()) {
+        JOptionPane.showMessageDialog(null, "Name must not be empty.");
+      } else {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+          public void run() {
+            new Client(username);
+          }
+        });
+        break;
+      }
+      
+    }
+  }
+
 }
--- Java/SocketProgramming/src/Server.java
+++ Java/SocketProgramming/src/Server.java
@@ -3,77 +3,86 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Server {
-	private ServerSocket server;
-	private PrintWriter output;
-	private BufferedReader input;
+  private static final List<Socket> CLIENT_LIST = new ArrayList<Socket>();
+  private static final List<String> USER_LIST = new ArrayList<String>();
 
-	public Server() {
-		try {
-			System.out.println("Server started...");
-			server = new ServerSocket(9000);
+  private ServerSocket server;
 
-			Socket client = server.accept();
-			System.out.println(client);
-			System.out.println("Client Above Connected");
+  public Server() {
+    try {
+      System.out.println("Server started...");
+      server = new ServerSocket(9000);
 
-			output = new PrintWriter(client.getOutputStream(), true);
-			input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+      while (true) {
+        Socket client = server.accept();
+        System.out.println(client);
+        System.out.println("Client Above Connected");
 
-			Thread inputReader = new Thread(new Runnable() {
-				public void run() {
-					try {
-						msgFromClient();
-					} catch (Exception e) {
-						System.exit(0);
-					}
-				}
-			});
+        setupClient(client);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 
-			Thread outputWriter = new Thread(new Runnable() {
-				public void run() {
-					try {
-						msgToClient();
-					} catch (Exception e) {
-						System.exit(0);
-					}
-				}
-			});
+  private void setupClient(Socket client) {
+    try {
+      CLIENT_LIST.add(client);
 
-			inputReader.start();
-			outputWriter.start();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+      final BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+      final String usr = input.readLine();
+      USER_LIST.add(usr);
+      printAllUser();
+      
+      Thread inputReader = new Thread(new Runnable() {
+        public void run() {
+          try {
+              msgFromClient(input, usr, client);
+          } catch (Exception e) {
+            e.printStackTrace();
+          }
+        }
+      });
 
-	private void msgFromClient() throws Exception {
-		String line = "";
-		while (line != null) {
-			line = input.readLine();
-			if (line != null) {
-				System.out.println("Client: " + line);
-			}
-		}
-		System.exit(0);
-	}
+      inputReader.start();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
 
-	private void msgToClient() throws Exception {
-		String line = "";
-		Scanner scanner = new Scanner(System.in);
-		while (!line.contains("exit")) {
-			line = scanner.nextLine();
-			if (!line.trim().isEmpty()) {
-				output.println(line);
-			}
-		}
-		scanner.close();
-		System.exit(0);
-	}
+  }
 
-	public static void main(String[] args) {
-		new Server();
-	}
+  private void msgFromClient(BufferedReader inputReader, String usr, Socket socket) throws Exception {
+    String line = "";
+    while (line != null) {
+      line = inputReader.readLine();
+      if (line != null) {
+        msgToAllClient(usr + ": " + line);
+      }
+    }
+    msgToAllClient("****** " + usr + " has left ******");
+    USER_LIST.remove(usr);
+    CLIENT_LIST.remove(socket);
+  }
+
+  private void msgToAllClient(String msg) throws Exception {
+    System.out.println("MSG: " + msg);
+    for (Socket client : CLIENT_LIST) {
+      PrintWriter outputWriter = new PrintWriter(client.getOutputStream(), true);
+      outputWriter.println(msg);
+    }
+  }
+  
+  private void printAllUser() throws Exception {
+    for (String user : USER_LIST) {
+      msgToAllClient("****** " + user + " has joined ******");
+    }
+  }
+
+  public static void main(String[] args) {
+    new Server();
+  }
 }
