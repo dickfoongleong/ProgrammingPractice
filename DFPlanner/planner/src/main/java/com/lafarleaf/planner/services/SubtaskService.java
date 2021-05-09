ae0e27c53a0546360407406f60918eeb14c901c8
DickFoong Leong <dickfoong0314@gmail.com>
2021-05-09 18:24:34 -0400

    Added Services.

---
+++ DFPlanner/planner/src/main/java/com/lafarleaf/planner/services/SubtaskService.java
@@ -0,0 +1,43 @@
+package com.lafarleaf.planner.services;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import com.lafarleaf.planner.models.Subtask;
+import com.lafarleaf.planner.models.Task;
+import com.lafarleaf.planner.repositories.SubtaskRepo;
+import com.lafarleaf.planner.utils.Exceptions.TaskNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SubtaskService {
+    private final SubtaskRepo repo;
+
+    @Autowired
+    public SubtaskService(SubtaskRepo repo) {
+        this.repo = repo;
+    }
+
+    public List<Subtask> findByTask(Task task) {
+        return repo.findByTask(task);
+    }
+
+    public Subtask findById(int id) throws TaskNotFoundException {
+        return repo.findById(id) // Find by Subtask ID
+                .orElseThrow(() -> // Throws error if not found.
+                new TaskNotFoundException("Subtask not found."));
+    }
+
+    @Transactional
+    public void add(Subtask subtask) {
+        repo.save(subtask);
+    }
+
+    @Transactional
+    public void delete(Subtask subtask) {
+        repo.delete(subtask);
+    }
+}
