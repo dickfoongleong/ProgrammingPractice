dae12ae35693d403e407b842d804d8b1d8601891
DickFoong Leong <dickfoong0314@gmail.com>
2021-05-09 17:48:07 -0400

    Added DB models.

---
+++ DFPlanner/planner/src/main/java/com/lafarleaf/planner/models/Subtask.java
@@ -0,0 +1,59 @@
+package com.lafarleaf.planner.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import org.hibernate.annotations.Type;
+
+@Entity(name = "subtask")
+public class Subtask {
+    @Id
+    @Column(name = "ID")
+    private int id;
+
+    @Column(name = "TITLE")
+    private String title;
+
+    @Column(name = "IS_DONE")
+    @Type(type = "org.hibernate.type.NumericBooleanType")
+    private boolean isDone;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "TASK_ID")
+    @JsonIgnore
+    private Task task;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean isDone) {
+        this.isDone = isDone;
+    }
+
+    public Task getTask() {
+        return task;
+    }
+
+    public void setTask(Task task) {
+        this.task = task;
+    }
+}
---
+++ DFPlanner/planner/src/main/java/com/lafarleaf/planner/models/User.java
@@ -0,0 +1,106 @@
+package com.lafarleaf.planner.models;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.hibernate.annotations.Type;
+
+@Entity(name = "user")
+public class User {
+    @Id
+    @Column(name = "ID")
+    private int id;
+
+    @Column(name = "FIRST_NAME")
+    private String fName;
+
+    @Column(name = "LAST_NAME")
+    private String lName;
+
+    @Column(name = "MIDDLE_NAME")
+    private String mName;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @Column(name = "IS_ENABLED")
+    @Type(type = "org.hibernate.type.NumericBooleanType")
+    private boolean isEnabled;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Task> tasks;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public String getmName() {
+        return mName == null ? "" : mName;
+    }
+
+    public String getFullName() {
+        String fullName = getfName() + " " + getlName() + " " + getmName();
+        return fullName.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+
+    public void setEnabled(boolean isEnabled) {
+        this.isEnabled = isEnabled;
+    }
+
+    public List<Task> getTaskList() {
+        return tasks;
+    }
+
+    public void setTaskList(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+        task.setUser(this);
+    }
+}
