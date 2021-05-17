d8068ecee9a1719645c499fb752208ce045a3ba8
DickFoong Leong <dickfoong0314@gmail.com>
2021-05-16 20:47:20 -0400

    Added POST, PUT, DELETE for tasks and subtasks

--- DFPlanner/planner/src/main/java/com/lafarleaf/planner/repositories/SubtaskRepo.java
+++ DFPlanner/planner/src/main/java/com/lafarleaf/planner/repositories/SubtaskRepo.java
@@ -1,6 +1,7 @@
 package com.lafarleaf.planner.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.lafarleaf.planner.models.Subtask;
 import com.lafarleaf.planner.models.Task;
@@ -9,4 +10,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SubtaskRepo extends JpaRepository<Subtask, Long> {
     List<Subtask> findByTask(Task task);
+
+    Optional<Subtask> findByTaskAndId(Task task, long id);
 }
