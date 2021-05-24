817710b6d5f463f6641465b1d2248eb6a0066d31
DickFoong Leong <dickfoong0314@gmail.com>
2021-05-24 07:28:01 -0400

    Init ReactJS app.

---
+++ DFPlanner/web/dfplanner-app/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className="headerDiv">
+            <h1>Header...</h1>
+        </header>
+    )
+}
+
+export default Header
